7993f3eaf3950e98c3a8908f6da9b5bb35795584
Aziz <med.aziz.gharbii@gmail.com>
2023-03-30 00:18:23 -0400

    fix: formatting

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import openai
 from typing import Any
 
 # Config
-from config import API_KEY,TOKEN,CHANNEL_ID
+from config import API_KEY, TOKEN, CHANNEL_ID
 
 # For a description of the Bot API, see this page: https://core.telegram.org/bots/api
 # Bot Examples
@@ -15,16 +15,14 @@ messages = []
 
 openai.api_key = API_KEY
 
-def make_request(prompt)-> str | None:
-    """ Make a request using open.ai ChatCompletion"""
-    messages.append(
-            {"role":"user","content":prompt}
-    )
 
-    response : Any = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", 
-        messages=messages
-    ) or None
+def make_request(prompt) -> str | None:
+    """Make a request using open.ai ChatCompletion"""
+    messages.append({"role": "user", "content": prompt})
+
+    response: Any = (
+        openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages) or None
+    )
 
     if response is not None:
         reply = response.get("choices")[0]["message"].content or None
@@ -33,29 +31,30 @@ def make_request(prompt)-> str | None:
 
     return response
 
-async def send_msg(msg)-> None:
+
+async def send_msg(msg) -> None:
     """Echo the assistant message."""
     await bot.send_message(chat_id=CHANNEL_ID, text=msg)
 
 
-async def echo(update: telegram.Update, _: ContextTypes.DEFAULT_TYPE)-> None:
+async def echo(update: telegram.Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
-    if update.channel_post :
-        reply =  make_request(update.channel_post.text)
+    if update.channel_post:
+        reply = make_request(update.channel_post.text)
         if reply is not None:
-            messages.append(
-                {"role":"assistant","content": reply}
-            ) 
+            messages.append({"role": "assistant", "content": reply})
             await send_msg(reply)
         else:
             await send_msg("Ask again!")
 
-def main()-> None:
+
+def main() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(TOKEN).build()
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     # Run the bot until the user presses Ctrl-C
     application.run_polling()
+
 
 main()
